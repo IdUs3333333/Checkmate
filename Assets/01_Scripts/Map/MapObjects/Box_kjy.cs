27203fcc343d35671429c448ad9a1d1ba8092fac
silbu123234 <a01056927631@gmail.com>
2025-05-09 12:44:05 +0900

    2025-05-09_1

---
+++ Assets/01_Scripts/Map/MapObjects/Box_kjy.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Box_kjy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
