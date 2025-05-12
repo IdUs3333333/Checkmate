26fd7231b4cca8e264da948937fad516b2121299
silbu123234 <a01056927631@gmail.com>
2025-05-12 16:40:12 +0900

    2025-05-12_1
    보스 맵 제작, 박스 스크립트 작성

--- Assets/01_Scripts/Map/MapObjects/Box_kjy.cs
+++ Assets/01_Scripts/Map/MapObjects/Box_kjy.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class Box_kjy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private int boxDieCount; //몇번 맞아야 부서지는지
+
+    private Rigidbody2D rigid;
+    private GameObject thisBox;
+
+    private int boxHitCount = 0; // 몇번 맞았는지
+
+    private void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+        thisBox = this.gameObject;
     }
 
-    // Update is called once per frame
-    void Update()
+    //공격을 맞을 때만 파괴되도록 코드 수정 요망
+    private void OnCollisionEnter2D(Collision2D collision)//충돌 체크
     {
-        
+        Debug.Log("충돌함");
+        boxHitCount++;//충돌 했음 증가
+        if(boxHitCount >= boxDieCount)//파괴 조건 만족 체크
+        {
+            Debug.Log("파괴됨");
+            Destroy(thisBox);//만족했음 박스 파괴
+        }
     }
 }
